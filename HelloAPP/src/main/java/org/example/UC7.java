25b6f4f14b552b739c31e302af0e6cf1f19c8194
githuabhavya <av6776@srmist.edu.in>
2026-03-26 15:30:58 +0530

    added UC7

---
+++ HelloAPP/src/main/java/org/example/UC7.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class UC7 {
+    public static void main(String[] args) {
+
+        String greeting;
+
+        // Check if arguments are provided
+        if (args.length == 0) {
+            greeting = "Hello, World!";
+        } else {
+            String names = String.join(", ", args);
+            greeting = "Hello, " + names + "!";
+        }
+
+        // Print result
+        System.out.println(greeting);
+    }
+}
